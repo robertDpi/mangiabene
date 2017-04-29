5051aebd77e638d2e17cddb7adcfafb2dc71c14f
Robert Pinna <robert.pinna.external@zalando.de>
2017-04-29 21:21:28 +0200

    cleaning up

--- dist/templates/index.js
+++ dist/templates/index.js
@@ -2,11 +2,12 @@ let mod = angular.module('wordpress-hybrid-client.overwriteTemplates', []);
 
 mod.run(($templateCache) => {
     // customPosts
-    $templateCache.put('customPosts/movie/item.html', require('!html!./customPosts/movie/item.html'));
-    $templateCache.put('customPosts/movie/list.html', require('!html!./customPosts/movie/list.html'));
+    // $templateCache.put('customPosts/movie/item.html', require('!html!./customPosts/movie/item.html'));
+    // $templateCache.put('customPosts/movie/list.html', require('!html!./customPosts/movie/list.html'));
+    $templateCache.put('customPosts/movie/menu.html', require('!html!./customPosts/movie/menu.html'));
 
     $templateCache.put('module/search.html', require('!html!./module/search.html'));
-    $templateCache.put('module/posts/list.html', require('!html!./module/posts-list.html'));
+    //$templateCache.put('module/posts/list.html', require('!html!./module/posts-list.html'));
 
     // BELOW ARE ALL THE TEMPLATES YOU CAN OVERWRITE IN WPHC
     // To do so go in the `lib/templates` folder, copy the template you want to modify
